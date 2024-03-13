13d2e9ca240274710550ba43cba9963ccb172601
Emanuele <emanuelemangano15@gmail.com>
2024-03-13 16:44:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("addTaskBtn")
+input.onclick = (event) => {
+  const taskToAdd = document.getElementById("newTask")
+  console.dir(taskToAdd)
+  const p = document.createElement("p")
+  p.innerText = taskToAdd.value
+  const main = document.getElementById("conteiner")
+  main.appendChild(p)
+}
+
+const input2 = document.getElementById("removeTaskBtn")
+input2.onclick = (event) => {
+  const toRemove = document.querySelector("p")
+  toRemove.remove()
+}
