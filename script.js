5355203cebc093f0c05bbeabde5084cd0a3df2ac
Emanuele <emanuelemangano15@gmail.com>
2024-03-25 08:46:07 +0100

    minor fix

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const input = document.getElementById("addTaskBtn")
+const input = document.getElementById("addTaskBtn");
 input.onclick = (event) => {
-  const taskToAdd = document.getElementById("newTask")
-  console.dir(taskToAdd)
-  const p = document.createElement("p")
-  p.innerText = taskToAdd.value
-  const main = document.getElementById("conteiner")
-  main.appendChild(p)
-}
+  const taskToAdd = document.getElementById("newTask");
+  console.dir(taskToAdd);
+  const p = document.createElement("p");
+  p.innerText = taskToAdd.value;
+  const main = document.getElementById("conteiner");
+  main.appendChild(p);
+};
 
-const input2 = document.getElementById("removeTaskBtn")
+const input2 = document.getElementById("removeTaskBtn");
 input2.onclick = (event) => {
-  const toRemove = document.querySelector("p")
-  toRemove.remove()
-}
+  const toRemove = document.querySelector("p");
+  toRemove.remove();
+};
+//ciao
